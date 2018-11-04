93fdf08fb632319ab171e6657cf7a269949b67f4
Bhavi Dhingra <bhavi.dhingra@students.iiit.ac.in>
2018-11-04 15:01:29 +0530

    grep working... '-n' flag enabled

--- bash.py
+++ bash.py
@@ -16,6 +16,9 @@ PWD="pwd"
 TOUCH="touch"
 GREP="grep"
 
+SUCCESS=0
+FAILURE=-1
+
 
 def PromptPrint():
     dt = date.today()
@@ -33,6 +36,39 @@ def PromptPrint():
     print ("@ ", end="")
     utils.ColorTextPrint(utils.GREEN, "[{0}] ".format(cwd), True)
     print ("$ ", end="")
+
+
+def RemovePrecedingSpaces(s):
+    while (s != "" and s[0] == ' '):
+        s = s[1:]
+    return s
+
+def RemoveTrailingSpaces(s):
+    n = len(s)
+    while (s != "" and s[n - 1] == ' '):
+        s = s[:n-1]
+        n -= 1
+    return s
+
+
+ # Returns the command and args strings
+def CommandExtract(s):
+    s = RemovePrecedingSpaces(s)
+
+    i = 0
+    while (i < len(s) and s[i] != ' '):
+        i += 1
+
+    if i == len(s):
+        return s, ""
+
+    cmd = s[:i]
+    args = s[i+1:]
+
+    args = RemovePrecedingSpaces(args)
+    args = RemoveTrailingSpaces(args)
+
+    return cmd, args
 
 
 def RemoveTrailingFwdSlashes(s):
@@ -63,17 +99,25 @@ def cd_run(args, prev_dir, curr_dir):
 
     elif (args[0] == '~'):   # handling "~/"
         args = TildeToHomeConvert(args)
-        print(args)
 
-    if (args[0] == '-'):     # handling '-'
+    elif (args[0] == '-'):     # handling '-'
         if prev_dir != "":
+            args = prev_dir
             print (prev_dir)
-            os.chdir(prev_dir)
-    else:
-        try:
-            os.chdir(args)
-        except FileNotFoundError:
-            utils.ErrorPrint("No such directory... please try again!!\n")
+        else:
+            args = curr_dir
+
+    elif (args[0] == "\""):
+        args = args[1:]
+        args = args[:len(args) - 1]
+
+    try:
+        os.chdir(args)
+    except Exception as err:
+        utils.ErrorPrint("cd: {0}\n".format(err))
+        return FAILURE
+
+    return SUCCESS
 
 
 def ls_run(args):
@@ -81,22 +125,38 @@ def ls_run(args):
         args = os.getcwd()
     elif (args[0] == '~'):
         args = TildeToHomeConvert(args)
+    elif (args[0] == "\""):
+        args = args[1:]
+        args = args[:len(args) - 1]
 
-    dir_content = os.listdir(args)
-    for c in sorted(dir_content):
-        if c[0] != '.':
-            print (c)
+    try:
+        dir_content = os.listdir(args)
+        for c in sorted(dir_content):
+            if c[0] != '.':
+                if (os.path.isdir("{0}/{1}".format(args, c))):
+                    utils.ColorTextPrint(utils.BLUE, "{0}\n".format(c), True)
+                else:
+                    print (c)
+    except Exception as err:
+        utils.ErrorPrint("ls: {0}\n".format(err))
+        
 
 def touch_run(args):
     if (args == ""):
         utils.ErrorPrint("touch: missing file operand\n")
         return
 
+    if (args[0] == '~'):
+        args = TildeToHomeConvert(args)
+    elif (args[0] == "\""):     # remove the double quotes
+        args = args[1:]
+        args = args[:len(args) - 1]
+
     # check whether the parent directories exist or not
     p = (str)(Path(args).absolute())
     basedir, f = os.path.split(p)
     if not Path(basedir).exists():
-        utils.ErrorPrint("touch: cannot touch {0}: no such file or directory!!\n".format(args))
+        utils.ErrorPrint("touch: cannot touch '{0}': no such file or directory!!\n".format(args))
         return
 
     # check whether the file/directory exists or not
@@ -108,65 +168,110 @@ def touch_run(args):
 
 
 def grep_file(pattern, filepath, flag_n, print_path = False):
-    with open(filepath, 'r') as search_file:
-        i = 0
-        for line in search_file:
-            i += 1
-            match = re.findall(r"{0}".format(pattern), line)
-            if match:
-                if print_path:
-                    if flag_n:
-                        print ("{0}:{1}:{2}".format(filepath, i, line), end="")
+    try:
+        with open(filepath, 'r') as search_file:
+            i = 0
+            for line in search_file:
+                i += 1
+                match = re.findall(r"{0}".format(pattern), line)
+                if match:
+                    if print_path:
+                        if flag_n:
+                            print ("{0}:{1}:{2}".format(filepath, i, line), end="")
+                        else:
+                            print ("{0}:{1}".format(filepath, i), end="")
                     else:
-                        print ("{0}:{1}".format(filepath, i), end="")
+                        if flag_n:
+                            print ("{0}:{1}".format(i, line), end="")
+                        else:
+                            print ("{0}".format(line), end="")
+
+    except Exception as err:
+        utils.ErrorPrint("grep: {0}: {1}\n".format(filepath, err))
+        
+
+def grep_dir_iterative(pattern, dirpath, flag_n):
+    try:
+        dir_content = os.listdir(dirpath)
+        for c in sorted(dir_content):
+            if c[0] != '.':
+                dest_path = "{0}/{1}".format(dirpath, c);
+                if (os.path.isdir(dest_path)):
+                    print("grep: {0}: Is a directory".format(dest_path))
                 else:
-                    if flag_n:
-                        print ("{0}:{1}".format(i, line), end="")
-                    else:
-                        print ("{0}".format(line), end="")
+                    grep_file(pattern, dest_path, flag_n, True)
+    except Exception as err:
+        utils.ErrorPrint("grep: {0}\n".format(err))
 
 
 def grep_run(args):
-    args_list = args.split()
-    n = len(args_list)
     flag_n = False
     flag_r = False
     pattern_found = False
     pattern = ""
+    search_dest = ""
 
-    for a in args_list:
-        if flag_n and flag_r:
-            break
-
-        if (a[0] == '-'):
-            for i in range(1, len(a)):
-                if (a[i] == 'n'):
-                    flag_n = True
-                elif (a[i] == 'r'):
+    i = 0
+    while (i < len(args)):
+        if(args[i] == '-'):
+            i += 1                      # get to next character
+            while(args[i] != ' '):
+                if (args[i] == 'r'):
                     flag_r = True
+                elif (args[i] == 'n'):
+                    flag_n = True
+                i += 1
 
-        elif (pattern_found == False):
-            pattern = a
+        elif(args[i] == ' '):           # ignore spaces
+            i += 1
+
+        elif (not pattern_found):
             pattern_found = True
+            if (args[i] == '\''):       # pattern is enclosed in single quotes ('<pattern>')
+                i += 1
+                while(args[i] != '\'' or args[i-1] == '\\'):    # the pattern may contain ' in itself
+                    if(args[i] != '\\'):
+                        pattern += str(args[i])
+
+                    i += 1
+
+                i += 1
+            else:
+                while(args[i] != ' '):
+                    pattern += str(args[i])
+                    i += 1
+
+                i += 1
+
+        else:                           # search dest is at the of the arguments
+            search_dest = args[i:]
+            i = len(args)
+
+            if (search_dest[0] == "\""):        # remove double quotes
+                search_dest = search_dest[1:]
+                search_dest = search_dest[:len(search_dest) - 1]
 
 
-    search_content = args_list[n-1]
-    if (search_content == "*"):
-        if flag_r:
-            grep_dir_recursive(pattern, os.getcwd(), flag_n)
-        else:
-            grep_dir_iterative(pattern, os.getcwd(), flag_n)
+    if (search_dest == "*"):
+        #if flag_r:
+        #    grep_dir_recursive(pattern, os.getcwd(), flag_n)
+        #else:
+        grep_dir_iterative(pattern, ".", flag_n)
 
-    elif (os.path.isdir(search_content)):
-        if not flag_r:
-            utils.ErrorPrint("grep: {0}: Is a directory\n".format(search_content))
+    elif (not Path(search_dest).exists()):
+        utils.ErrorPrint("grep: '{0}': no such file or directory\n".format(search_dest))
+        return
+
+    else: 
+        if(os.path.isdir(search_dest)):
+            #if not flag_r:
+            utils.ErrorPrint("grep: '{0}': Is a directory\n".format(search_dest))
             return
-        else:
-            grep_dir_recursive(pattern, search_content, flag_n)
+            #else:
+            #    grep_dir_recursive(pattern, search_dest, flag_n)
 
-    elif (os.path.isfile(search_content)):
-        grep_file(pattern, search_content, flag_n)
-
+        elif (os.path.isfile(search_dest)):
+            grep_file(pattern, search_dest, flag_n)
 
 
 if __name__ == "__main__":
@@ -179,21 +284,24 @@ if __name__ == "__main__":
         PromptPrint()
         ip_cmd = input()
 
-        cmd_list = ip_cmd.split()
-        if (len(cmd_list) == 0):
+        cmd, args = CommandExtract(ip_cmd)
+        #cmd_list = ip_cmd.split()
+        #if (len(cmd_list) == 0):
+        if (cmd == ""):
             continue
 
-        if (cmd_list[0] == EXIT):
+        #if (cmd_list[0] == EXIT):
+        if (cmd == EXIT):
             break
 
-        cmd = cmd_list[0]
-        del cmd_list[0]
-        args = ' '.join([str(a) for a in cmd_list])
+        #cmd = cmd_list[0]
+        #del cmd_list[0]
+        #args = ' '.join([str(a) for a in cmd_list])
 
         if (cmd == CD):
-            cd_run(args, prev_dir, curr_dir)
-            prev_dir = curr_dir
-            curr_dir = os.getcwd()
+            if cd_run(args, prev_dir, curr_dir) == SUCCESS:
+                prev_dir = curr_dir
+                curr_dir = os.getcwd()
 
         elif (cmd == LS):
             ls_run(args)
@@ -208,7 +316,7 @@ if __name__ == "__main__":
             grep_run(args)
 
         else:
-            utils.ErrorPrint ("{0}: command not found\n".format(cmd))
+            utils.ErrorPrint ("'{0}': command not found\n".format(cmd))
 
     utils.screen_clear()
     utils.BoldPrint("Pyshell Exited...\n\n")
